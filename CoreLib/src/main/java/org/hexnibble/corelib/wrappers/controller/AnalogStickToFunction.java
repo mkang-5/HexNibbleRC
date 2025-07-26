ea9f4db1cd83dc89b4b60aa7fed2ddad7bfb1657
mkang <min.kang5@gmail.com>
2025-07-26 12:32:28 -0700

    update

--- CoreLib/src/main/java/org/hexnibble/corelib/wrappers/controller/AnalogStickToFunction.java
+++ CoreLib/src/main/java/org/hexnibble/corelib/wrappers/controller/AnalogStickToFunction.java
@@ -3,13 +3,6 @@ package org.hexnibble.corelib.wrappers.controller;
 import org.hexnibble.corelib.misc.FloatConsumer;
 
 public record AnalogStickToFunction(ControllerWrapper.ANALOG_STICK analogStickName, FloatConsumer functionToRun) {
-  /**
-   * Simple constructor. This one assumes the button is newly pressed and no shift button is
-   * pressed.
-   *
-   * @param analogStickName
-   * @param functionToRun
-   */
   public AnalogStickToFunction {
   }
 }
